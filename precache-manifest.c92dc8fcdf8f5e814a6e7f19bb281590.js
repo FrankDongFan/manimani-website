e35fe59c264b2322f10d44ccc29b4087ca348dcb
frank <970359440@qq.com>
2022-05-26 15:05:07 +0800

    deploy

---
+++ precache-manifest.c92dc8fcdf8f5e814a6e7f19bb281590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb2d9992ee1149553f1",
+    "url": "/manimani-website/css/app.097104ef.css"
+  },
+  {
+    "revision": "862a420c84761cf1a7c401e9cd39e36f",
+    "url": "/manimani-website/index.html"
+  },
+  {
+    "revision": "abb2d9992ee1149553f1",
+    "url": "/manimani-website/js/app.83c1e42a.js"
+  },
+  {
+    "revision": "941e00476034976af54c",
+    "url": "/manimani-website/js/chunk-vendors.729c5ff9.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/manimani-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/manimani-website/robots.txt"
+  }
+]);
